8df7fce42110c116de0e8bca7ae5413dd9b1028c
Veer Jain <111719388+veerjain1@users.noreply.github.com>
2022-08-22 21:51:00 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,105 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  string jokestart;
+
+    Console.Write(" Knock Knock! \n>>");
+    //store response as readline
+    jokestart = Console.ReadLine();//saved user input
+
+    Console.WriteLine("Nobel");
+    
+
+    string askwho;//create variable for when they ask who is the person
+
+    Console.Write(">>");
+    askwho = Console.ReadLine();
+
+    Console.WriteLine("Nobel... Thats why I knocked. \n\t\t\tClick Enter for anothe amazing joke...");
+    Console.ReadLine();// user needs to click enter for next joke
+
+    //----------Next Question
+
+    string joke2start;
+
+    Console.Write(" Knock Knock! \n>>");
+    //store response as readline
+    joke2start = Console.ReadLine();//saved user input
+
+    Console.WriteLine("Figs");
+    
+
+    string askwho2;//create variable for when they ask who is the person
+
+    Console.Write(">>");
+    askwho2 = Console.ReadLine();
+
+    Console.WriteLine("Figs the doorbell... Its broken! \n\t\t\tClick Enter for anothe amazing joke...");
+    Console.ReadLine();// user needs to click enter for next joke
+
+    //----------Next Question
+
+
+    string joke3start;
+
+    Console.Write(" Knock Knock! \n>>");
+    //store response as readline
+    joke3start = Console.ReadLine();//saved user input
+
+    Console.WriteLine("orange");
+    
+
+    string askwho3;//create variable for when they ask who is the person
+
+    Console.Write(">>");
+    askwho3 = Console.ReadLine();
+
+    Console.WriteLine("Orange you glad you met me? :) \n\t\t\tClick enter for another amazing joke...");
+    Console.ReadLine();// user needs to click enter for next joke
+
+    //----------Next Question
+
+    string joke4start;
+
+    Console.Write(" Knock Knock! \n>>");
+    //store response as readline
+    joke4start = Console.ReadLine();//saved user input
+
+    Console.WriteLine("Olive");
+    
+
+    string askwho4;//create variable for when they ask who is the person
+
+    Console.Write(">>");
+    askwho4 = Console.ReadLine();
+
+    Console.WriteLine("Olive you :) \n\t\t\tClick Enter for another amazing joke...");
+    Console.ReadLine();// user needs to click enter for next joke
+
+    //----------Next Question
+
+
+    string joke5start;
+
+    Console.Write(" Knock Knock! \n>>");
+    //store response as readline
+    joke5start = Console.ReadLine();//saved user input
+
+    Console.WriteLine("Dwayne");
+    
+
+    string askwho5;//create variable for when they ask who is the person
+
+    Console.Write(">>");
+    askwho5 = Console.ReadLine();
+
+    Console.WriteLine("Dwayne the sink! I need to use it! \n\tTHATS ALL THE JOKES FOR NOW...FOR MORE JOKES, VENMO ME 5$ AN HOUR. THANKS!");
+    //Console.ReadLine();// user needs to click enter for next joke
+
+    //----------Next Question
+  
+
+
+  }
+}
